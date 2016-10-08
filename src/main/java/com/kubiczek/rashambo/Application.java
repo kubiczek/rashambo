5b8c0585a11006b8d26ac92d085b8680cc60b422
Marcin Kubiczek <marcin.kubiczek@barklayscorp.com>
2016-10-08 12:00:07 +0100

    initial set-up of the project

---
+++ src/main/java/com/kubiczek/rashambo/Application.java
@@ -0,0 +1,14 @@
+package com.kubiczek.rashambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mkubiczek on 10/8/2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
